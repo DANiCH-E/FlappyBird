d534501ec40c5d53cef40b336c1c4c283aff9856
Daniil <90925204+DANiCH-E@users.noreply.github.com>
2024-03-09 14:18:46 +0300

    add bird movement

---
+++ Assets/Scripts/Bird/BirdMover.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class BirdMover : MonoBehaviour
+{
+    [SerializeField] private Vector3 _startPosition;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _tapForce = 10f;
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _maxRotationZ;
+    [SerializeField] private float _minRotationZ;
+
+    private Rigidbody2D _rigidbody;
+    private Quaternion _maxrotarion;
+    private Quaternion _minrotarion;
+
+    private void Start()
+    {
+        transform.position = _startPosition;
+
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.velocity = Vector2.zero;
+
+        _maxrotarion = Quaternion.Euler(0, 0, _maxRotationZ);
+        _minrotarion = Quaternion.Euler(0, 0, _minRotationZ);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidbody.velocity = new Vector2(_speed, 0); //гасить скорость, чтобы не улетал слишком высоко
+            transform.rotation = _maxrotarion;
+            _rigidbody.AddForce(Vector2.up * _tapForce, ForceMode2D.Force);
+        }
+        transform.rotation = Quaternion.Lerp(transform.rotation, _minrotarion, _rotationSpeed * Time.deltaTime);
+    }
+}
